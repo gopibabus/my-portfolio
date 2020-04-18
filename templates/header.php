92feceac56b1207a4eea021cc28c0320c07682f3
Gopi Srungavarapu <Gopi.Srungavarapu@rotary.org>
2020-04-18 12:55:40 -0500

    added cdn dependencies

--- templates/header.php
+++ templates/header.php
@@ -4,19 +4,20 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content="" />
+    <meta name="description" content="portfolio gopibabu" />
     <meta name="author" content="" />
     <link rel="icon" href="./img/favicon.ico" type="image/ico" sizes="48x48" />
 
-    <title>Portfolio - Gopibabu</title>
+    <title>Portfolio</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.0/css/bootstrap.min.css"
+        rel="stylesheet" />
 
     <!-- Custom fonts for this template -->
     <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" />
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet" />
 
     <!-- Custom styles for this template -->
     <link href="css/resume.css" rel="stylesheet" />
