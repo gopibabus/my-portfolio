5ef3d34de5947a6b0070148725f1b8b1fe81df7f
Gopibabu Srungavarapu <s.gopibabu@gmail.com>
2020-05-21 11:54:09 -0500

    updated job description

--- templates/experience.php
+++ templates/experience.php
@@ -4,6 +4,17 @@
         <h2 class="mb-5">Experience</h2>
 
         <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto" id="rotary-job">
+                <h3 class="mb-0" id="rotary-job-title"></h3>
+                <div class="subheading mb-3" id="rotary-job-client"></div>
+                <p id="rotary-job-description"></p>
+            </div>
+            <div class="resume-date text-md-right">
+                <span class="text-primary" id="rotary-job-duration"></span>
+            </div>
+        </div>
+
+        <div class="resume-item d-flex flex-column flex-md-row mb-5">
             <div class="resume-content mr-auto" id="disney-job">
                 <h3 class="mb-0" id="disney-job-title"></h3>
                 <div class="subheading mb-3" id="disney-job-client"></div>
