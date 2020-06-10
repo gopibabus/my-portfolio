ebf322920d9a6f8b2895ef541298dd7f92b4736c
Gopibabu Srungavarapu <s.gopibabu@gmail.com>
2020-06-09 22:24:50 -0500

    added job description

--- js/content.js
+++ js/content.js
@@ -7,48 +7,47 @@ const jobItems = {
 
 	'rotary-job-title': 'Sr.PHP Developer',
 	'rotary-job-client': 'Rotary International, Evanston, IL',
-	'rotary-job-description': `Working on application called <a href="https://my.rotary.org/en" target="_blank"><b>My Rotary</b></a>, 
-	which is an efficient tool designed to serve those Rotarians 
-	who have a Rotary account. Among other features, the site allows maintaining a user’s profile as well as search for clubs and
-	members. Additional functionality provided to Club officers for member maintenance. This project aimed to decrease Rotarian
-	task time for club administration efforts by 15%, reduce the number of access permissions complaints by 10% within 12 months
-	of a new release.`,
+	'rotary-job-description': `Working on application called <a href="https://my.rotary.org/en" target="_blank"><strong>My Rotary</strong></a>, which is an efficient tool designed to serve those Rotarians who have a Rotary account. 
+	Among other features, the site allows maintaining a user’s profile as well as search for clubs and members. Additional functionality provided to
+	Club officers for member maintenance. This project aimed to decrease Rotarian task time for club administration efforts by 15%, reduce the number of
+	access permissions complaints by 10% within 12 months of a new release.`,
 	'rotary-job-duration': 'May 2019 – present',
 
 	'disney-job-title': 'Software Applications Developer',
 	'disney-job-client': 'Walt Disney Attractions Technology, Orlando, FL',
-	'disney-job-description': `Worked on a customer-facing <a href="https://disneyworld.disney.go.com/" target="_blank"><b>Web Application</b></a>, which focuses on providing alternative resorts and rooms 
-	to the customer. The primary goal of this application is to provide alternative rooms and resorts to the customer who is having
-	an existing reservation with Disney resorts. This application helps Disney to divert traffic from a busy resort to other available
-	resorts.`,
+	'disney-job-description': `Worked on a customer-facing <a href="https://disneyworld.disney.go.com/" target="_blank"><strong>Web Application</strong></a>, which focuses on providing alternative resorts and rooms to the customer. 
+	The primary goal of this application is to provide alternative rooms and resorts to the customer who is having an existing reservation with Disney resorts.
+	This application helps Disney to divert traffic from a busy resort to other available resorts.`,
 	'disney-job-duration': 'February 2018 – May 2019',
 
 	'nytimes-job-title': 'Software Engineer',
 	'nytimes-job-client': 'New York Times, New York, NY',
-	'nytimes-job-description': `Worked on an e-commerce customer care project, which is an <b>Intranet application</b> for the New York Times 
-	customer care division. This project focuses on the development and maintaining the most innovative and affordable CRM solution
-	for the New York Times, which provides subscription information of different types of customers in different formats
-	(documents, pie charts, etc.,). This application allows us to add, remove and update customer information.`,
+	'nytimes-job-description': `Worked on an e-commerce customer care project, which is an <strong>Intranet application</strong> for the New York Times 
+	customer care division. This project focuses on the constructing and maintaining the most innovative and affordable CRM solution for the New York Times,
+	which provides subscription information of different types of customers in different formats (documents, pie charts, etc.,). This application allows
+	customer care representatives to add, remove and update customer information related to their subscriptions.`,
 	'nytimes-job-duration': 'July 2016 – January 2018',
 
 	'studyhour-job-title': 'PHP Developer',
 	'studyhour-job-client': 'Study hour, Boston, MA',
-	'studyhour-job-description': `<a href="http://studyhour.com/" target="_blank"><b>Study hour</b></a> is an online Teaching website that provides a wide range of courses on a variety of topics 
-	to students at home convenience. It instantly connects tutors and students over online messaging and tutoring calls.
-	Users post their questions and receive an answer from tutors. This application supports group tutoring sessions.`,
+	'studyhour-job-description': `<a href="http://studyhour.com/" target="_blank"><strong>Study hour</strong></a> is an online Teaching platform that provides a wide range of 
+	courses on a variety of topics to students at home convenience. It instantly connects tutors and students over online messaging and tutoring calls. Users can post
+	their questions and can receive answers from tutors. This application supports group tutoring sessions.`,
 	'studyhour-job-duration': 'May 2015 - July 2016',
 
 	'ascendLearning-job-title': 'PHP Developer',
 	'ascendLearning-job-client': 'Ascend Learning, Boston, MA',
-	'ascendLearning-job-description': `<a href="https://www.medhub.com/" target="_blank"><b>Med Hub</b></a> is a project that provides data-driven, educational solutions that accelerate learning and impact job readiness, employment success and employee retention. MedHub solutions enable streamlined departmental workflow, flexible curriculum, and outcome mapping, robust trainee & faculty portfolio management, meaningful assessment, faculty evaluation and seamless integration with other campus systems.`,
+	'ascendLearning-job-description': `<a href="https://www.medhub.com/" target="_blank"><strong>Med Hub</strong></a> is a project that provides data-driven, educational solutions 
+	that accelerate learning and impact job readiness, employment success and employee retention. MedHub solutions enable streamlined departmental workflow, flexible
+	curriculum, and outcome mapping, robust trainee & faculty portfolio management, meaningful assessment, faculty evaluation and seamless integration with other campus
+	systems.`,
 	'ascendLearning-job-duration': 'Jan 2013 - Dec 2014',
 
 	'popcorn-job-title': 'PHP Developer',
 	'popcorn-job-client': 'Popcorn Apps, Hyderabad, INDIA',
-	'popcorn-job-description': `Popcorn Apps is the leading e-commerce and web service provider supporting Multi-store Multi-shop systems 
-	and online shopping for various clients worldwide, especially for online sales, E-commerce web solutions and secured payment checkouts.
-	This project is an <b>Intranet web application</b>, which is responsible for generating different formats of data like pie charts, excel documents
-	 on internal employee data. `,
+	'popcorn-job-description': `Popcorn Apps is the leading e-commerce and web service provider supporting Multi-store Multi-shop systems and online shopping for 
+	various clients worldwide, especially for online sales, E-commerce web solutions. This project is an <strong>Intranet web application</strong>, which is responsible for creating
+	different formats of data like pie charts, excel documents on internal employee data. `,
 	'popcorn-job-duration': 'Jan 2011 - Dec 2012',
 };
 
